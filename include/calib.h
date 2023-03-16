1f1dac000c97abe0176085eb86459baa17c8fb6b
Abinav Anantharaman <abinav2695@gmail.com>
2023-03-16 00:59:48 -0400

    Project Repo Init

---
+++ include/calib.h
@@ -0,0 +1,11 @@
+/* Abinav Anantharaman and Satwik Bhandiwad
+   CS 5330 Spring 2023
+   Calibration Header File
+*/
+
+#ifndef CALIB_H
+#define CALIB_H
+
+
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+/* Abinav Anantharaman and Satwik Bhandiwad
+   CS 5330 Spring 2023
+   Real Time Object Recognition Test Code File
+   Source file
+*/
+
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+#include "calib.h"
+
+
+int main()
+{
+    return 0;
+}
+
